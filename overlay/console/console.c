da0fa2ad26f44d79ee8147ba87f3fd57ee179430
SYFaren <petkovmatvej@gmail.com>
2026-08-31 15:14:35 +0500

    Fix musl console build without glibc linux/kd.h headers.
    Define KDGKBMODE/KDSKBMODE/K_UNICODE when __MUSL__ is set; glibc builds
    keep including linux/kd.h as before.

--- overlay/console/console.c
+++ overlay/console/console.c
@@ -12,7 +12,14 @@
 #include <unistd.h>
 
 #ifdef __linux__
+#if defined(__MUSL__)
+/* musl-gcc lacks glibc's linux/*.h layout; ioctl numbers are stable. */
+#define KDGKBMODE 0x4B44
+#define KDSKBMODE 0x4B45
+#define K_UNICODE 0x03
+#else
 #include <linux/kd.h>
+#endif
 #endif
 
 typedef struct {
